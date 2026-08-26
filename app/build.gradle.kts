b3b5fee0a06420c140fc5b01c86f25f65c54b808
Steph@RobotFury <206148058+RoboDroidPro@users.noreply.github.com>
2026-08-26 09:08:19 -0600

    Changed gradle:app to use jvmToolChain. tested. works

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,17 +30,8 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-//    kotlinOptions {
-//        jvmTarget = "11"
-//    }
     kotlin {
-        compilerOptions {
-            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
-        }
+        jvmToolchain(11)
     }
     buildFeatures {
         compose = true
